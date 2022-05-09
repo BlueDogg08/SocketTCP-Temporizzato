6adea6e48146d713f6ae662e68e1af418b7af3d5
BlueDogg08 <94381985+BlueDogg08@users.noreply.github.com>
2022-05-09 16:58:08 +0200

    Update GestoreServer.java

--- src/GestoreServer.java
+++ src/GestoreServer.java
@@ -3,5 +3,6 @@ public class GestoreServer {
     public static void main(String[] args) {
         Server srv = new Server(2000);
         srv.inAscolto();
+        srv.scrivi("messaggio");
     }
 }
