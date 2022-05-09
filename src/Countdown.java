9167aa0fef68374521151642d0a2b25a77ee4f76
BlueDogg08 <94381985+BlueDogg08@users.noreply.github.com>
2022-05-09 16:57:16 +0200

    Update Countdown.java

--- src/Countdown.java
+++ src/Countdown.java
@@ -1,3 +1,24 @@
-public class Countdown extends Thread{
-    
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Countdown extends Thread {
+
+    private int tempo;
+
+    public Countdown(int t) {
+        this.tempo = t;
+    }
+
+    @Override
+    public void run() {
+        for (int i = tempo; i > 0; i--) {
+            try {
+                System.out.println(i);
+                sleep(1000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Countdown.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
 }
